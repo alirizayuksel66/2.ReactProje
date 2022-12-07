1c39ea264f40892018736333b1edac26cf747498
Ali Rıza Yüksel <114735512+alirizayuksel66@users.noreply.github.com>
2022-12-07 16:10:25 +0300

    useRef ve forwardRef
    useRef ve forwardRef İle İlgili Örnekler Yapıldı

--- src/Test.js
+++ src/Test.js
@@ -1,18 +1,41 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import { useRef } from "react"
 
 export default function Test() {
 
+    const [postID, setPostID] = useState(1)
+    const [post, setPost] = useState(false)
+    const yaziRef = useRef()
+    const focusYazi = () => {
+        yaziRef.current.focus()
+    }
     useEffect(() => {
         console.log('Component İlk Yüklendiğinde Çalışır')
+        return() => {
+            console.log('Component Destroyed')
+        }
     },[])
 
     useEffect(() =>{
+        fetch(`https://www.youtube.com/${postID}`)/*Link Eklenicek*/
+        .then(res => res.json())
+        .then(data => setPost(data))
+    }, [postID])
+
+    /*
+    useEffect(() =>{
         console.log('Component Render Oldu!')
     })
-
+    */
 
     return (
         <div>
+            <input ref={yaziRef}></input>
+            <h3>{postID}</h3>
+            {post && JSON.stringify(post)}
+            <button onClick={() => setPostID(postID => postID + 1)}>Arttır</button>
+            <button onClick={focusYazi}>Odakla</button>
+            <hr></hr>
             Test Komponenti
         </div>
     )
